4497bc7d8b56f77bc002d5d74aebd0c56f4f3950
alexander <alexandernyaga1999@gmail.com>
2024-01-04 11:07:43 +0300

    Define Hiker,Equipment and trip

--- database.py
+++ database.py
@@ -6,3 +6,24 @@ engine = create_engine('sqlite:///hikers_management.db', echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
+class Hiker(Base):
+    __tablename__ = 'hikers'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    contact_info = Column(String)
+    emergency_contact = Column(String)
+
+class Equipment(Base):
+    __tablename__ = 'equipment'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    condition = Column(String)
+
+class Trip(Base):
+    __tablename__ = 'trips'
+
+    id = Column(Integer, primary_key=True)
+    trip_name = Column(String, nullable=False)
