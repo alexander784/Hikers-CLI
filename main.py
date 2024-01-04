65b5cba13bafe7a59e88f1ffbe5e68c5615440d7
alexander <alexandernyaga1999@gmail.com>
2024-01-04 11:31:10 +0300

    Add main script to main.py

--- main.py
+++ main.py
@@ -40,5 +40,5 @@ def main():
             print("Invalid choice. Please enter a valid option.")
 
 
-            if __name__ == "__main__":
-                 main()
+if __name__ == "__main__":
+    main()
