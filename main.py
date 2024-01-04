fbbf98838e660a91d52c71ac78ddcdbc849a7469
alexander <alexandernyaga1999@gmail.com>
2024-01-04 11:17:00 +0300

    Add a main function 'initialoize database

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from create_hiker import create_hiker
 from create_equipment import create_equipment
 from plan_trip import plan_trip
 from database import initialize_database
+
+def main():
+    initialize_database()
