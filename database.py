9e53192473027f007004f5541ca557a6c52194dc
alexander <alexandernyaga1999@gmail.com>
2024-01-04 11:05:51 +0300

    Create and engine and session

--- database.py
+++ database.py
@@ -1,3 +1,8 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///hikers_management.db', echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
