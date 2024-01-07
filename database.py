a14851eb38cca5ffc402b67825ec9013b81c666f
alexander <alexandernyaga1999@gmail.com>
2024-01-07 22:09:59 +0300

    Create User class for database model

--- database.py
+++ database.py
@@ -7,6 +7,13 @@ Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
 class Hiker(Base):
     __tablename__ = 'hikers'
 
